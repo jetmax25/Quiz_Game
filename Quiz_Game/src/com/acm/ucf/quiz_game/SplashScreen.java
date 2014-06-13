2e0dde5027a8d7efb3fe4563f343c2cb4dc531f0
Nick Love <nicklove2011@gmail.com>
2014-06-13 14:53:13 -0400

    Added splashscreen

---
+++ Quiz_Game/src/com/acm/ucf/quiz_game/SplashScreen.java
@@ -0,0 +1,37 @@
+package com.acm.ucf.quiz_game;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+	
+	// Splash screen timer
+		private static int SPLASH_TIME_OUT = 2000;
+
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.activity_splash);
+
+			Thread startTimer = new Thread(){ 
+				public void run() {
+					try {
+						sleep(5000);
+						Intent i = new Intent(SplashScreen.this, MenuActivity.class);
+						startActivity(i);
+						finish();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+			};
+			startTimer.start();
+			
+		}
+
+}
